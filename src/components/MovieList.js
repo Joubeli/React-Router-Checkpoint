f3a834002137eba2e63d1d488b2eb676131f5ec6
imene joubeli <joubeliimen@gmail.com>
2021-05-31 17:51:54 +0100

    first Router commit

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -12,8 +12,7 @@ const List = ({movies, input, rateFilter }) => {
                    { movies.filter(el => el.titre.toLowerCase().includes(input.toLowerCase().trim())&& el.rating >=rateFilter.toString()).map((movie, index) => (
                         <Moviecard movie={movie} />))
                     }
-            
-
+        
             </div>
 
 
